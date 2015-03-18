5e9584c7fa428134401fbd1ee6dac62c7853f981
Sam Ternent <st@st-mac.local>
2015-03-18 16:29:18 +0000

    Adds travis ci

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    var browsers = [{
+        browserName: "firefox",
+        version: "19",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "XP"
+    }, {
+        browserName: "internet explorer",
+        platform: "XP",
+        version: "8"
+    }];
+
+    grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: "",
+          port: 9999
+        }
+      }
+    },
+    'saucelabs-jasmine': {
+        all :{
+            options: {
+                urls: ['http://127.0.0.1:9999/spec/SpecRunner.html'],
+                tunnelTimeout: 5,
+                build: process.env.TRAVIS_JOB_ID,
+                concurrency: 3,
+                browsers: browsers,
+                testname: 'yaks_testing',
+                tags: ['master']
+            }
+        }
+    },
+    watch: {},
+    browserify: {
+       yaks: {
+        files: {
+         'spec/yaks_build.js': ['lib/index.js'],
+         'spec/yaks_test.js': ['spec/yaks/**/*.js.coffee'  ],
+        },
+        options: {
+          transform: ['coffeeify'],
+        }
+      },
+
+    }
+    });
+
+    // Loading dependencies
+    for (var key in grunt.file.readJSON("package.json").devDependencies) {
+      if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
+    }
+    
+    grunt.registerTask("dev", ["browserify", "connect", "watch"]);
+    grunt.registerTask("test", ["browserify", "connect", "saucelabs-jasmine"]);
+};
